b4938b3e9daa3e8951dd377fbfe812edc2742429
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-02-15 17:37:33 +0600

    edited

--- index.ts
+++ index.ts
@@ -3,24 +3,7 @@ const a: string = "Hello world!";
 // string, number, Array, null, undefined, bollean, never, unknown
 // any
 
-// function getLowerCase(str: string) {
-//   return str.toLowerCase();
-// }
-
-// console.log(getLowerCase("Hello"));
-// console.log(getLowerCase("Hello world!"));
-// console.log(getLowerCase(""));
-
-// const b: number = 234;
-
-// function add(number1: number = 23, number2: number): number {
-//   return number1 + number2;
-// }
-
-// const substract = (number1: number, number2: number): number => {
-//   return number1 - number2;
-// };
-
+// function z
 substract(34, 23);
 add(3, 4);
 add(54, b);
